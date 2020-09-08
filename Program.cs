396f235cac08bc149b015384a9f628428197112f
Soumya121099 <70697901+Soumya121099@users.noreply.github.com>
2020-09-08 16:41:32 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var book = new Book("Soumya's grade book");
+            book.AddGrade(90.9);
+            book.AddGrade(89.0);
+            book.AddGrade(91.9);
+            book.ShowStatistics();
+            
+            
+        }
+    }
+}
